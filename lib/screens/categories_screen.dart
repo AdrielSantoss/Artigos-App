16f1d0c6651ad1e2ca0d460bfb5f75cce43a7958
AdrielSantoss <adrieldossantos7@gmail.com>
2022-01-29 11:04:38 -0300

    Adiciona novo layout para a aplicação.

--- lib/screens/categories_screen.dart
+++ lib/screens/categories_screen.dart
@@ -1,18 +1,29 @@
+import 'package:articles/components/category_item.dart';
+import 'package:articles/data/dummy_data.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({ Key? key }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return GridView(
-      gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-        maxCrossAxisExtent: 200,
-        childAspectRatio: 3 / 2,
-        crossAxisSpacing: 20,
-        mainAxisSpacing: 20
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Tecnologias'),
       ),
-      children: [],
+      body: GridView(
+        padding: const EdgeInsets.all(25),
+        gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
+          maxCrossAxisExtent: 200,
+          childAspectRatio: 3 / 2,
+          crossAxisSpacing: 20,
+          mainAxisSpacing: 20
+        ),
+        children: catogories.map((category) {
+          return CategoryItem(category: category);
+        }).toList(),
+      ),
     );
   }
 }
