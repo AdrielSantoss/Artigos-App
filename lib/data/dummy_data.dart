fd06ffa24b74065dca32e360a123751a17715d91
AdrielSantoss <adrieldossantos7@gmail.com>
2022-01-29 10:24:26 -0300

    first commit

---
+++ lib/data/dummy_data.dart
@@ -0,0 +1,31 @@
+
+import 'package:articles/models/category.dart';
+import 'package:flutter/material.dart';
+
+List<Category> catogories = [
+   Category(
+    id: 'a1',
+    title: 'Programação',
+    color: Colors.purple
+  ),
+  Category(
+    id: 'a2',
+    title: 'Redes',
+    color: Colors.purple
+  ),
+  Category(
+    id: 'a3',
+    title: 'Hardware',
+    color: Colors.purple
+  ),
+  Category(
+    id: 'a4',
+    title: 'Design',
+    color: Colors.purple
+  ),
+  Category(
+    id: 'a6',
+    title: 'Marketing digital',
+    color: Colors.purple
+  )
+];
